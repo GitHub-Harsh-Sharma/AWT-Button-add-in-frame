a95953cdbb4b335c0ce712986204f52ff2cb0787
Harsh Sharma <117378407+GitHub-Harsh-Sharma@users.noreply.github.com>
2023-03-05 15:02:36 +0530

    Add files via upload

---
+++ button.java
@@ -0,0 +1,28 @@
+package myflowlayout;
+import java.awt.*;
+class MyFlowLayout
+{
+    Frame f;
+    MyFlowLayout()
+    {
+        f=new Frame("hello");
+        Button b1=new Button("1");
+        Button b2=new Button("2");
+        Button b3=new Button("3");
+        Button b4=new Button("4");
+        Button b5=new Button("5");
+        f.add(b1);
+        f.add(b2);
+        f.add(b3);
+        f.add(b4);
+        f.add(b5);
+        FlowLayout f1=new FlowLayout(FlowLayout.LEFT,20,20);
+        f.setLayout(f1);
+        f.setVisible(true);
+        f.setSize(300,300);
+    }
+    public static void main(String[] args) 
+    {
+        MyFlowLayout mf=new MyFlowLayout();
+    }
+}
